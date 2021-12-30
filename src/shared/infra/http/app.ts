60a988f100b0f059198388c60d466062a4a12379
Sérgio Fleury Dias Filho <sergiofdf@gmail.com>
2021-12-30 00:21:43 -0300

    Cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -10,6 +10,8 @@ import upload from "@config/upload";
 import { AppError } from "@shared/errors/AppError";
 import createConnection from "@shared/infra/typeorm";
 
+import cors from "cors";
+
 import swaggerFile from "../../../swagger.json";
 import { router } from "./routes";
 
@@ -23,6 +25,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
+app.use(cors());
 app.use(router);
 
 app.use(
